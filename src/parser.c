050fc5be896f829025ed05a94a9d9ea1e2dfebef
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-03-08 15:24:59 +0100

    split parse functions

--- src/parser.c
+++ src/parser.c
@@ -3,135 +3,137 @@
 #include "art32/parser.h"
 #include "art32/convert.h"
 
-a32_parser_err_t a32_parser_next(a32_parser_t* p, a32_parser_code_t* c) {
+static a32_parser_err_t a32_parser_next_binary(a32_parser_t* p, a32_parser_code_t* c) {
   // clear code
   memset(c, 0, sizeof(a32_parser_code_t));
 
-  // parse binary
-  if (p->binary) {
-    // check offset
-    if (p->off == p->length) {
-      return A32_PARSER_ERR_DONE;
-    }
-
-    // set offset
-    c->off = p->off;
-
-    // get num
-    uint8_t num = p->source[p->off];
-
-    // find def
-    a32_parser_def_t* def = NULL;
-    for (int i = 0; i < p->num_defs; i++) {
-      if (num == p->defs[i].num) {
-        def = &p->defs[i];
-      }
-    }
-    if (def == NULL) {
-      return A32_PARSER_ERR_UNKNOWN;
-    }
-
-    // set def
-    c->def = def;
-
-    // increment
-    p->off++;
-
-    // parse arguments
-    for (int i = 0; def->fmt[i] != 0; i++) {
-      switch (def->fmt[i]) {
-        case 'o': {
-          // check length
-          if (p->length - p->off < 1) {
-            return A32_PARSER_ERR_OVERFLOW;
-          }
-
-          // set value
-          c->args[i].o = p->source[p->off];
-
-          // increment
-          p->off += 1;
-
-          break;
-        }
-        case 'i': {
-          // check length
-          if (p->length - p->off < 4) {
-            return A32_PARSER_ERR_OVERFLOW;
-          }
-
-          // set value
-          c->args[i].i = *((int32_t*)(p->source + p->off));
-
-          // increment
-          p->off += 4;
-
-          break;
-        }
-        case 'l': {
-          // check length
-          if (p->length - p->off < 8) {
-            return A32_PARSER_ERR_OVERFLOW;
-          }
-
-          // set value
-          c->args[i].l = *((int64_t*)(p->source + p->off));
-
-          // increment
-          p->off += 8;
-
-          break;
-        }
-        case 'f': {
-          // check length
-          if (p->length - p->off < 4) {
-            return A32_PARSER_ERR_OVERFLOW;
-          }
-
-          // set value
-          c->args[i].f = *((float*)(p->source + p->off));
-
-          // increment
-          p->off += 4;
-
-          break;
-        }
-        case 'd': {
-          // check length
-          if (p->length - p->off < 8) {
-            return A32_PARSER_ERR_OVERFLOW;
-          }
-
-          // set value
-          c->args[i].d = *((double*)(p->source + p->off));
-
-          // increment
-          p->off += 8;
-
-          break;
-        }
-        case 's': {
-          // get length
-          size_t len = strlen((char*)(p->source + p->off));
-
-          // check length
-          if (len >= p->length - p->off) {
-            return A32_PARSER_ERR_OVERFLOW;
-          }
-
-          // set value
-          c->args[i].s = (char*)(p->source + p->off);
-
-          // increment
-          p->off += len + 1;
-
-          break;
-        }
-      }
-    }
-
-    return A32_PARSER_ERR_OK;
+  // check offset
+  if (p->off == p->length) {
+    return A32_PARSER_ERR_DONE;
   }
+
+  // set offset
+  c->off = p->off;
+
+  // get num
+  uint8_t num = p->source[p->off];
+
+  // find def
+  a32_parser_def_t* def = NULL;
+  for (int i = 0; i < p->num_defs; i++) {
+    if (num == p->defs[i].num) {
+      def = &p->defs[i];
+    }
+  }
+  if (def == NULL) {
+    return A32_PARSER_ERR_UNKNOWN;
+  }
+
+  // set def
+  c->def = def;
+
+  // increment
+  p->off++;
+
+  // parse arguments
+  for (int i = 0; def->fmt[i] != 0; i++) {
+    switch (def->fmt[i]) {
+      case 'o': {
+        // check length
+        if (p->length - p->off < 1) {
+          return A32_PARSER_ERR_OVERFLOW;
+        }
+
+        // set value
+        c->args[i].o = p->source[p->off];
+
+        // increment
+        p->off += 1;
+
+        break;
+      }
+      case 'i': {
+        // check length
+        if (p->length - p->off < 4) {
+          return A32_PARSER_ERR_OVERFLOW;
+        }
+
+        // set value
+        c->args[i].i = *((int32_t*)(p->source + p->off));
+
+        // increment
+        p->off += 4;
+
+        break;
+      }
+      case 'l': {
+        // check length
+        if (p->length - p->off < 8) {
+          return A32_PARSER_ERR_OVERFLOW;
+        }
+
+        // set value
+        c->args[i].l = *((int64_t*)(p->source + p->off));
+
+        // increment
+        p->off += 8;
+
+        break;
+      }
+      case 'f': {
+        // check length
+        if (p->length - p->off < 4) {
+          return A32_PARSER_ERR_OVERFLOW;
+        }
+
+        // set value
+        c->args[i].f = *((float*)(p->source + p->off));
+
+        // increment
+        p->off += 4;
+
+        break;
+      }
+      case 'd': {
+        // check length
+        if (p->length - p->off < 8) {
+          return A32_PARSER_ERR_OVERFLOW;
+        }
+
+        // set value
+        c->args[i].d = *((double*)(p->source + p->off));
+
+        // increment
+        p->off += 8;
+
+        break;
+      }
+      case 's': {
+        // get length
+        size_t len = strlen((char*)(p->source + p->off));
+
+        // check length
+        if (len >= p->length - p->off) {
+          return A32_PARSER_ERR_OVERFLOW;
+        }
+
+        // set value
+        c->args[i].s = (char*)(p->source + p->off);
+
+        // increment
+        p->off += len + 1;
+
+        break;
+      }
+    }
+  }
+
+  return A32_PARSER_ERR_OK;
+}
+
+static a32_parser_err_t a32_parser_next_string(a32_parser_t* p, a32_parser_code_t* c) {
+  // clear code
+  memset(c, 0, sizeof(a32_parser_code_t));
 
   // prepare code token
   char* code_token = NULL;
@@ -228,5 +230,13 @@ a32_parser_err_t a32_parser_next(a32_parser_t* p, a32_parser_code_t* c) {
     }
 
     return A32_PARSER_ERR_OK;
+  }
+}
+
+a32_parser_err_t a32_parser_next(a32_parser_t* p, a32_parser_code_t* c) {
+  if (p->binary) {
+    return a32_parser_next_binary(p, c);
+  } else {
+    return a32_parser_next_string(p, c);
   }
 }
