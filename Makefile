001bbe506e09b39b2785a2524f397feef0fcf0b4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-03-08 10:38:52 +0100

    test numbers

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 fmt:
-	clang-format -i ./main.c ./src/*.c ./include/art32/*.h -style="{BasedOnStyle: Google, ColumnLimit: 120, SortIncludes: false}"
+	clang-format -i ./main.c ./src/*.c ./include/art32/*.h ./tests/*.cpp -style="{BasedOnStyle: Google, ColumnLimit: 120, SortIncludes: false}"
---
+++ tests/numbers.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <art32/numbers.h>
+}
+
+TEST(Numbers, Constrain) {
+  EXPECT_EQ(2, a32_constrain_i(1, 2, 3));
+  EXPECT_EQ(2, a32_constrain_l(1, 2, 3));
+  EXPECT_EQ(2, a32_constrain_f(1, 2, 3));
+  EXPECT_EQ(2, a32_constrain_d(1, 2, 3));
+
+  EXPECT_EQ(3, a32_constrain_i(4, 2, 3));
+  EXPECT_EQ(3, a32_constrain_l(4, 2, 3));
+  EXPECT_EQ(3, a32_constrain_f(4, 2, 3));
+  EXPECT_EQ(3, a32_constrain_d(4, 2, 3));
+}
+
+TEST(Numbers, Map) {
+  EXPECT_EQ(50, a32_map_i(5, 0, 10, 0, 100));
+  EXPECT_EQ(50, a32_map_l(5, 0, 10, 0, 100));
+  EXPECT_EQ(50, a32_map_f(5, 0, 10, 0, 100));
+  EXPECT_EQ(50, a32_map_d(5, 0, 10, 0, 100));
+
+  EXPECT_EQ(10, a32_map_i(1, 3, 8, 30, 80));
+  EXPECT_EQ(10, a32_map_l(1, 3, 8, 30, 80));
+  EXPECT_EQ(10, a32_map_f(1, 3, 8, 30, 80));
+  EXPECT_EQ(10, a32_map_d(1, 3, 8, 30, 80));
+
+  EXPECT_EQ(100, a32_map_i(10, 3, 8, 30, 80));
+  EXPECT_EQ(100, a32_map_l(10, 3, 8, 30, 80));
+  EXPECT_EQ(100, a32_map_f(10, 3, 8, 30, 80));
+  EXPECT_EQ(100, a32_map_d(10, 3, 8, 30, 80));
+}
+
+TEST(Numbers, SafeMap) {
+  EXPECT_EQ(50, a32_safe_map_i(5, 0, 10, 0, 100));
+  EXPECT_EQ(50, a32_safe_map_l(5, 0, 10, 0, 100));
+  EXPECT_EQ(50, a32_safe_map_f(5, 0, 10, 0, 100));
+  EXPECT_EQ(50, a32_safe_map_d(5, 0, 10, 0, 100));
+
+  EXPECT_EQ(30, a32_safe_map_i(1, 3, 8, 30, 80));
+  EXPECT_EQ(30, a32_safe_map_l(1, 3, 8, 30, 80));
+  EXPECT_EQ(30, a32_safe_map_f(1, 3, 8, 30, 80));
+  EXPECT_EQ(30, a32_safe_map_d(1, 3, 8, 30, 80));
+
+  EXPECT_EQ(80, a32_safe_map_i(10, 3, 8, 30, 80));
+  EXPECT_EQ(80, a32_safe_map_l(10, 3, 8, 30, 80));
+  EXPECT_EQ(80, a32_safe_map_f(10, 3, 8, 30, 80));
+  EXPECT_EQ(80, a32_safe_map_d(10, 3, 8, 30, 80));
+}
