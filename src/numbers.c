c888a8a2bd79d2b984cfa2b5fdb7cccca87516e7
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-04-23 11:13:44 +0200

    performance fix

--- src/numbers.c
+++ src/numbers.c
@@ -25,17 +25,21 @@ double a32_map_d(double num, double in_min, double in_max, double out_min, doubl
 }
 
 int32_t a32_safe_map_i(int32_t num, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max) {
-  return A32_CONSTRAIN(A32_MAP(num, in_min, in_max, out_min, out_max), out_min, out_max);
+  int32_t res = A32_MAP(num, in_min, in_max, out_min, out_max);
+  return A32_CONSTRAIN(res, out_min, out_max);
 }
 
 int64_t a32_safe_map_l(int64_t num, int64_t in_min, int64_t in_max, int64_t out_min, int64_t out_max) {
-  return A32_CONSTRAIN(A32_MAP(num, in_min, in_max, out_min, out_max), out_min, out_max);
+  int64_t res = A32_MAP(num, in_min, in_max, out_min, out_max);
+  return A32_CONSTRAIN(res, out_min, out_max);
 }
 
 float a32_safe_map_f(float num, float in_min, float in_max, float out_min, float out_max) {
-  return A32_CONSTRAIN(A32_MAP(num, in_min, in_max, out_min, out_max), out_min, out_max);
+  float res = A32_MAP(num, in_min, in_max, out_min, out_max);
+  return A32_CONSTRAIN(res, out_min, out_max);
 }
 
 double a32_safe_map_d(double num, double in_min, double in_max, double out_min, double out_max) {
-  return A32_CONSTRAIN(A32_MAP(num, in_min, in_max, out_min, out_max), out_min, out_max);
+  double res = A32_MAP(num, in_min, in_max, out_min, out_max);
+  return A32_CONSTRAIN(res, out_min, out_max);
 }
